b1bed77ba34afdcca783cb17474efe772be9d0ce
Kirianto <k.d.irianto@uii.ac.id>
2021-02-23 15:51:36 +0700

    added code examples for week 1

---
+++ code-examples/week1/blink-1.ino
@@ -0,0 +1,15 @@
+int led1 = 12;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(led1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
---
+++ code-examples/week1/blink-2.ino
@@ -0,0 +1,27 @@
+int led1 = 12;
+int led2 = 11;
+int led3 = 10;
+int led4 = 9;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
